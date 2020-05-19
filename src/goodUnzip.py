561458515e5108708ec1ed5784ccdfa5d2ba1ad8
JuliusJacobitz <julius@jacobitz.de>
2020-05-19 20:06:21 +0200

    works on win 10 and python 3.8

---
+++ src/goodUnzip.py
@@ -0,0 +1,44 @@
+from subprocess import call
+from time import sleep
+from pathlib import Path
+import os
+import zipfile
+
+formats = [".zip", ".rar"]
+cwd = os.getcwd()
+new_wd = None
+
+for entry in os.scandir():
+    if not entry.is_dir():
+        file_path = Path(entry.name)
+        file_name, file_format = os.path.splitext(str(file_path))
+        whole_path = cwd + r"\{0}".format(file_path)
+        if file_format in formats:
+            try:
+                os.mkdir(cwd + r"\{0}".format(file_name))
+                new_wd = cwd + r"\{0}".format(file_name)
+                print("> created folder: {0}".format(new_wd))
+                try:
+                    with zipfile.ZipFile(whole_path, 'r') as zip_ref:
+                        zip_ref.extractall(new_wd)
+                    print("> transfer completed")
+
+                    try:
+                        checker = input(r"Delete Zip ? 'n' for no")
+                        if checker != "n":
+                            os.remove(whole_path)
+                            print("> removed zip folder: {0}".format(file_path))
+                    except Exception as e:
+                        print(e)
+                        # error handling :D
+
+                except Exception as e:
+                    print(e)
+                    # error handling
+
+            except Exception as e:
+                print(e)
+                # später vlt ja gibt den namen schon haben backup gemacht
+
+print("done!")
+sleep(0.3)
---
+++ src/key_creator.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import winreg as reg
+
+cwd = os.getcwd()
+
+python_exe = sys.executable
+
+key_path = r"Directory\\Background\\shell\\PythonTestingKey1"
+
+key = reg.CreateKey(reg.HKEY_CLASSES_ROOT, key_path)
+
+reg.SetValue(key, '', reg.REG_SZ, '&Good Unzip!')
+
+key1 = reg.CreateKey(key, r"command")
+reg.SetValue(key1, '', reg.REG_SZ, python_exe + ' "' + cwd + r"\goodUnzip.py" + '"')     # important str has to look like: ...python.exe "..src/test.py"
+
+
+#print(python_exe + ' "'+cwd+r"\testKey.py"+'"')
