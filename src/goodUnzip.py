279519a814c7c5222d5a756381a47d8471ae2256
JuliusJacobitz <julius@jacobitz.de>
2020-05-19 23:56:56 +0200

    minor beautify

--- src/goodUnzip.py
+++ src/goodUnzip.py
@@ -33,23 +33,21 @@ for entry in os.scandir():
                 createBackup(1)
 
             try:
-                print("  ---- ", new_wd)
                 with zipfile.ZipFile(whole_path, 'r') as zip_ref:
                     zip_ref.extractall(new_wd)
                 print("> transfer completed")
 
                 try:
-                    checker = input(r"Delete Zip ? 'n' for no")
+                    checker = input(r"Delete Zip? Insert 'n' for no: ")
                     if checker != "n":
                         os.remove(whole_path)
                         print("> removed zip folder: {0}".format(file_path))
                 except Exception as e:
                     print(e)
-                    # error handling :D
+                    # error handling 
             except Exception as e:
                     print(e)
                     # error handling
 
-
 print("> done!")
-sleep(0.3)
+sleep(0.2)
