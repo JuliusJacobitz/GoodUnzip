40a88eee59b11c41a3e5542bfb7900676c5542be
JuliusJacobitz <julius@jacobitz.de>
2020-05-21 19:29:53 +0200

    finished statistics

--- src/goodUnzip.py
+++ src/goodUnzip.py
@@ -9,22 +9,36 @@ formats = [".zip", ".rar"]
 cwd = os.getcwd()
 new_wd = None
 
-try:
-    with open() as json_file:
-        data = json.load(json_file)
-        for i in data:
-            print(i)
-except Exception as e:
-    print(e)
 
-# todo
+def openStatJsonR():
+    try:
+        with open(str(os.path.dirname(os.path.abspath(__file__))) + r"\statistics.txt") as json_file:
+            statisticData = json.load(json_file)
+            return statisticData
+    except Exception as e:
+        print(e)
+
+
 def add_del_folders_stat(amount):
-    pass
+    try:
+        statData = openStatJsonR()
+        statData["folders_deleted"] += amount
+        with open(str(os.path.dirname(os.path.abspath(__file__))) + r"\statistics.txt", 'w') as outfile:
+            json.dump(statData, outfile)
+
+    except Exception as e:
+        print(e)
 
 
-# todo
 def add_unzipped_stat(amount):
-    pass
+    try:
+        statData = openStatJsonR()
+        statData["data_unzipped"] += amount
+        with open(str(os.path.dirname(os.path.abspath(__file__))) + r"\statistics.txt", 'w') as outfile:
+            json.dump(statData, outfile)
+
+    except Exception as e:
+        print(e)
 
 
 def createBackup(c):
@@ -62,7 +76,7 @@ for entry in os.scandir():
                     if checker != "n":
                         os.remove(whole_path)
                         print("> removed zip folder: {0}".format(file_path))
-                        add_del_folders_stat()
+                        add_del_folders_stat(1)
 
                 except Exception as e:
                     print(e)
