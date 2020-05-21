f0a32785977a88e70c41c7ca2dfe72e4012883ec
JuliusJacobitz <julius@jacobitz.de>
2020-05-21 19:07:31 +0200

    added statistics

--- src/goodUnzip.py
+++ src/goodUnzip.py
@@ -3,10 +3,29 @@ from time import sleep
 from pathlib import Path
 import os
 import zipfile
+import json
 
 formats = [".zip", ".rar"]
 cwd = os.getcwd()
 new_wd = None
+
+try:
+    with open() as json_file:
+        data = json.load(json_file)
+        for i in data:
+            print(i)
+except Exception as e:
+    print(e)
+
+# todo
+def add_del_folders_stat(amount):
+    pass
+
+
+# todo
+def add_unzipped_stat(amount):
+    pass
+
 
 def createBackup(c):
     global new_wd
@@ -15,14 +34,15 @@ def createBackup(c):
         new_wd = cwd + r"\{0}_{1}".format(file_name, c)
         print("> created backup folder: {0}".format(new_wd))
     except:
-        createBackup(c+1)
+        createBackup(c + 1)
+
 
 for entry in os.scandir():
     if not entry.is_dir():
         file_path = Path(entry.name)
         file_name, file_format = os.path.splitext(str(file_path))
         whole_path = cwd + r"\{0}".format(file_path)
-        
+
         if file_format in formats:
             try:
                 os.mkdir(cwd + r"\{0}".format(file_name))
@@ -35,20 +55,22 @@ for entry in os.scandir():
                 with zipfile.ZipFile(whole_path, 'r') as zip_ref:
                     zip_ref.extractall(new_wd)
                 print("> transfer completed")
+                add_unzipped_stat(1)
 
                 try:
                     checker = input(r"> Delete Zip? Insert 'n' for no: ")
                     if checker != "n":
                         os.remove(whole_path)
                         print("> removed zip folder: {0}".format(file_path))
+                        add_del_folders_stat()
 
                 except Exception as e:
                     print(e)
                     # error handling
                 print("\n")
             except Exception as e:
-                    print(e)
-                    # error handling
+                print(e)
+                # error handling
 
 print("> done!")
-sleep(0.1)
+sleep(1000)
